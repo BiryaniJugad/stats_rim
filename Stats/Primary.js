aa986862f03035fa1ec33c6a56e05b9a49bfa38f
EbonyEdge Solutions <css.jaspherjosephg05@gmail.com>
2026-02-18 13:29:58 +0800

    Convert Primary class from C# to JavaScript

---
+++ Stats/Primary.js
@@ -0,0 +1,11 @@
+// Primary.js
+export class Primary {
+    constructor(str, agi, vit, intel, dex, luk) {
+        this.STR = str;
+        this.AGI = agi;
+        this.VIT = vit;
+        this.INT = intel;
+        this.DEX = dex;
+        this.LUK = luk;
+    }
+}
