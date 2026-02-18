61ba33c7eaebac273c5adfc844a604e3e4d8b04d
JohnNheil12 <pallaset01@gmail.com>
2026-02-18 09:22:51 +0800

    some change on the ui

--- Form1.cs
+++ Form1.cs
@@ -14,8 +14,8 @@ namespace stats_rim
         private async void Form1_Load(object sender, EventArgs e)
         {
             await webView1.EnsureCoreWebView2Async(null);
-            webView1.ZoomFactor = .95;
-            webView1.CoreWebView2.Settings.IsZoomControlEnabled = false;
+            //webView1.ZoomFactor = .60;
+            //webView1.CoreWebView2.Settings.IsZoomControlEnabled = false;
             string htmlPath = Path.Combine(Application.StartupPath, "UI", "index.html");
 
             if (File.Exists(htmlPath))
